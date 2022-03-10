b036abf08e058b71c0b712f5832d6b3b13facc7a
Cristian Di Carlo <cristiandcbusiness@gmail.com>
2022-03-10 22:12:19 +0100

    update comments

--- To Do List/script.js
+++ To Do List/script.js
@@ -6,33 +6,33 @@ var ul = document.querySelector("ul");
 
 var everyLi = document.querySelectorAll("li"); 				// main array with all "li" elements
 
-everyLi.forEach(liFix); 									// loop telling all "li" elements to exec "liFix" function
+everyLi.forEach(liFix); 						// loop telling all "li" elements to exec "liFix" function
 
-function liFix(elements) {                          		// function telling "elements" of the "everyLi" array to:
+function liFix(elements) {                          			// function telling "elements" of the "everyLi" array to:
 	elements.addEventListener("click", function(){  		// listen any click on any "element" and exec the function
 		elements.classList.toggle("done");          		// toggle class "done" on clicked "element"
 	})
 	var btn = document.createElement("button");     		// variable declaring "btn" as a creating button element
-	btn.innerText = "Delete";								// telling the creating button to have "Delete" as text
-	elements.appendChild(btn);								// appending new "button" element as child of "li" parent Node
-	btn.addEventListener("click", function(){				// listen any click on any "btn" element and exec the function
-			btn.parentNode.remove();						// remove the parent Node (li) on clicked "btn"
+	btn.innerText = "Delete";					// telling the creating button to have "Delete" as text
+	elements.appendChild(btn);					// appending new "button" element as child of "li" parent Node
+	btn.addEventListener("click", function(){			// listen any click on any "btn" element and exec the function
+			btn.parentNode.remove();			// remove the parent Node (li) on clicked "btn"
 		})
 }
 
-function createListElement() {								// base function of the exercise edited to support new features
+function createListElement() {						// base function of the exercise edited to support new features
 	var li = document.createElement("li");
-	var btn = document.createElement("button");				// variable declaring "btn" as a creating button element
+	var btn = document.createElement("button");			// variable declaring "btn" as a creating button element
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
-	li.addEventListener("click", function() {				// listen any click on any "li" element and exec the function
-		li.classList.toggle("done");						// toggle class "done" on clicked "li" element
+	li.addEventListener("click", function() {			// listen any click on any "li" element and exec the function
+		li.classList.toggle("done");				// toggle class "done" on clicked "li" element
 	});
-	btn.innerText = "Delete";								// telling the creating button to have "Delete" as text
-	li.appendChild(btn);									// appending new "button" element as child of "li" parent Node
-	btn.addEventListener("click", function(){				// listen any click on any "btn" element and exec the function
-			btn.parentNode.remove();						// remove the parent Node (li) on clicked "btn"
+	btn.innerText = "Delete";					// telling the creating button to have "Delete" as text
+	li.appendChild(btn);						// appending new "button" element as child of "li" parent Node
+	btn.addEventListener("click", function(){			// listen any click on any "btn" element and exec the function
+			btn.parentNode.remove();			// remove the parent Node (li) on clicked "btn"
 		})
 }
 
