395a08b5eb64f640c953a44c827ce43659ca1e58
Okazakee <cristiandcbusiness@gmail.com>
2022-03-13 18:18:38 +0100

    finished exercise, polishing needed

--- Color Picker JS/script.js
+++ Color Picker JS/script.js
@@ -1,19 +1,39 @@
+// 1. Write code so that the colour inputs match the background generated on the first page load. 
+// 2. Display the initial CSS linear gradient property on page load.
+// 3. BONUS: Add a random button which generates two random numbers for the colour inputs.
+
+// initial exercise vars notouch
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
-function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+// personal vars
+var button = document.getElementById("btn");
 
+
+// 1. and 2.- Function call to override default color inputs values at loading, passively completing second task.
+setGradient();
+
+
+// main exercise function
+function setGradient() {
+	body.style.background = "linear-gradient(to right, " + color1.value	+ ", " + color2.value + ")";
 	css.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient);
+// personal function to get and apply random color to css
+function getRandColor() {
+	// stolen variable for random color
+	var randomColor1 = '#'+Math.floor(Math.random()*16777215).toString(16);
+	var randomColor2 = '#'+Math.floor(Math.random()*16777215).toString(16);
+    body.style.background = "linear-gradient(to right, " + randomColor1	+ ", " + randomColor2 + ")";
+	css.textContent = body.style.background + ";";
+}
 
+
+// main exercise base calls
+color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+// personal calls
+button.addEventListener("click", getRandColor);
